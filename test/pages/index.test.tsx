e279ee0a36d5f7f1dc20524f290299151a2e4a5d
Davy Hausser <davy.hausser@gmail.com>
2020-09-28 17:05:05 +0200

    :white_check_mark: update of test suite

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -3,11 +3,25 @@ import { render, fireEvent } from '@testing-library/react'
 import Index from '../../pages/index'
 
 describe('Home page', () => {
-  it('renders deploy link', () => {
+  it('sign up page title', () => {
     const { getByText } = render(<Index />)
-    const linkElement = getByText('Sign Up')
-    console.log(linkElement)
-    expect(linkElement).toBeInTheDocument()
+    const titleElement = getByText('Sign Up')
+    console.log(titleElement)
+    expect(titleElement).toBeInTheDocument()
+  })
+
+  it('first name input field', () => {
+    const { getByText } = render(<Index />)
+    const inputElement = getByText('First Name:')
+    console.log(inputElement)
+    expect(inputElement).toBeInTheDocument()
+  })
+
+  it('last name input field', () => {
+    const { getByText } = render(<Index />)
+    const inputElement = getByText('Last Name:')
+    console.log(inputElement)
+    expect(inputElement).toBeInTheDocument()
   })
 
   it('matches snapshot', () => {
