1582c3b50173278fb0e6ae6cee1661e114884dfc
Davy Hausser <davy.hausser@gmail.com>
2021-04-24 09:21:12 +0200

    react hook form update to v7

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,40 @@
-import { useForm } from 'react-hook-form'
-import { ErrorMessage } from '../components/errorMessage'
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "../components/errorMessage";
 
 export interface FormInputs {
-  firstName: string
-  lastName: string
-  gender: string
-  username: string
-  email: string
-  age: number
-  about: string
+  firstName: string;
+  lastName: string;
+  gender: string;
+  username: string;
+  email: string;
+  age: number;
+  about: string;
 }
 
 function App() {
   const {
     register,
     handleSubmit,
-    errors,
     setError,
     clearErrors,
-    formState: { isSubmitting },
-  } = useForm<FormInputs>()
+    formState: { errors, isSubmitting },
+  } = useForm<FormInputs>();
   const onSubmit = (data: FormInputs) => {
-    alert(JSON.stringify(data))
-  }
+    alert(JSON.stringify(data));
+  };
   const sleep = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms))
+    new Promise((resolve) => setTimeout(resolve, ms));
   const validateUserName = async (value: string) => {
-    await sleep(1000)
-    if (value !== 'bill') {
-      setError('username', { types: { validate: true } })
-      return true
+    await sleep(1000);
+    if (value !== "bill") {
+      setError("username", { types: { validate: true } });
+      return true;
     }
     {
-      clearErrors('username')
-      return false
+      clearErrors("username");
+      return false;
     }
-  }
+  };
 
   return (
     <form className="App" onSubmit={handleSubmit(onSubmit)}>
@@ -43,22 +42,22 @@ function App() {
 
       <button
         onClick={() => {
-          window.alert('With typescript and Jest')
+          window.alert("With typescript and Jest");
         }}
       >
         Test Button
       </button>
 
       <label>First Name</label>
-      <input name="firstName" ref={register({ required: true })} />
+      <input {...register("firstName", { required: true })} />
       <ErrorMessage error={errors.firstName} />
 
       <label>Last Name</label>
-      <input name="lastName" ref={register({ required: true })} />
+      <input {...register("lastName", { required: true })} />
       <ErrorMessage error={errors.lastName} />
 
       <label>Gender</label>
-      <select name="gender" ref={register({ required: true })}>
+      <select {...register("gender", { required: true })}>
         <option>Select...</option>
         <option>Male</option>
         <option>Female</option>
@@ -67,33 +66,30 @@ function App() {
 
       <label>Username</label>
       <input
-        name="username"
-        onBlur={(e) => validateUserName(e.target.value)}
-        ref={register({ required: true, validate: validateUserName })}
+        // onBlur={(e) => validateUserName(e.target.value)}
+        {...register("username", {
+          required: true,
+          validate: validateUserName,
+        })}
       />
       <ErrorMessage error={errors.username} />
 
       <label>Email</label>
       <input
-        name="email"
-        ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
       />
       <ErrorMessage error={errors.email} />
 
       <label>Age</label>
-      <input
-        name="age"
-        type="number"
-        ref={register({ required: true, min: 18 })}
-      />
+      <input type="number" {...register("age", { required: true, min: 18 })} />
       <ErrorMessage error={errors.age} />
 
       <label>About you</label>
-      <textarea name="about" ref={register} />
+      <textarea {...register("about")} />
 
       <input disabled={isSubmitting} type="submit" />
     </form>
-  )
+  );
 }
 
-export default App
+export default App;
